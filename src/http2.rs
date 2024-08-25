448aa8a906bca168458927c1a8db2d08a06d588b
kamran <kamrad1372@gmail.com>
2024-08-26 00:10:55 +0330

    add http2 pri

--- src/http2.rs
+++ src/http2.rs
@@ -1,9 +1,24 @@
-pub mod payload_flags;
-pub mod payload;
 pub mod frame;
 pub mod hpack;
+pub mod payload;
+pub mod payload_flags;
+
+use std::str::FromStr;
 
 pub use frame::*;
 pub use hpack::*;
-pub use payload_flags::*;
 pub use payload::*;
+pub use payload_flags::*;
+
+const DEFAULT_PRI: &str = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n";
+pub struct Http2Pri {
+    content: String,
+}
+
+impl From<Vec<u8>> for Http2Pri {
+    fn from(value: Vec<u8>) -> Self {
+        Self {
+            content: String::from_str(std::str::from_utf8(&value[0..24]).unwrap()).unwrap(),
+        }
+    }
+}
