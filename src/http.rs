5e28b4c8de15f4c4efdcc9fbf319e452b8387223
kamran <kamrad1372@gmail.com>
2024-11-11 14:32:58 +0330

    next step

--- src/http.rs
+++ src/http.rs
@@ -1,3 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
+// https://documentation.softwareag.com/webmethods/cloudstreams/wst10-5/10-5_CloudStreams_webhelp/index.html#page/cloudstreams-webhelp/to-custom_connector_15.html
+// https://greenbytes.de/tech/webdav/rfc2388.xhtml#n-sets-of-files
+// https://www.rfc-editor.org/rfc/inline-errata/rfc7578.html
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+
 pub mod http;
 pub mod http_message;
 pub mod request;
