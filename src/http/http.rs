510fbb17665e77934c02fd8d14b35592706cf74c
kamran <kamrad1372@gmail.com>
2024-11-27 16:32:24 +0330

    next step

--- src/http/http.rs
+++ src/http/http.rs
@@ -2,22 +2,38 @@ use crate::Result::{self, Err, Ok};
 use core::str;
 use std::collections::HashMap;
 use std::fmt::{self, Display};
-use std::str::FromStr;
+use std::str::{FromStr, Utf8Error};
 
 #[derive(Debug)]
-pub struct ParseHeadersError(String);
+pub enum ParseHttpError {
+    ParseHeaderError(String),
+    ParseFormDataError(String),
+    UnknownString(String),
+}
 
-impl fmt::Display for ParseHeadersError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Invalid header: {}", self.0)
+impl From<&str> for ParseHttpError {
+    fn from(residual: &str) -> Self {
+        ParseHttpError::UnknownString(String::from(residual))
     }
 }
 
-impl std::error::Error for ParseHeadersError {}
+impl From<Utf8Error> for ParseHttpError {
+    fn from(residual: Utf8Error) -> Self {
+        ParseHttpError::UnknownString("Invalid Utf-8".to_string())
+    }
+}
+
+impl fmt::Display for ParseHttpError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid header: {}", self)
+    }
+}
+impl std::error::Error for ParseHttpError {}
 
 macro_rules! define_headers {
     ($($variant:ident => $name:expr),*) => {
         #[derive(Debug)]
+        #[allow(non_camel_case_types)]
         pub enum StandardHeaders {
             $($variant),*
         }
@@ -31,12 +47,12 @@ macro_rules! define_headers {
         }
 
         impl FromStr for StandardHeaders {
-            type Err = ParseHeadersError;
+            type Err = ParseHttpError;
 
             fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
                 match s {
                     $($name => std::result::Result::Ok(StandardHeaders::$variant),)*
-                    _ => std::result::Result::Err(ParseHeadersError(s.to_string())),
+                    _ => std::result::Result::Err(ParseHttpError::ParseHeaderError(s.to_string())),
                 }
             }
         }
@@ -90,24 +106,25 @@ define_headers! {
     // Add the rest as required
 }
 
-// pub type CustomHeader = String;
 pub struct CustomHeader(String);
 pub enum HeaderKey {
     StandardHeader(StandardHeaders),
     CustomHeader(CustomHeader),
 }
-
 impl FromStr for HeaderKey {
-    type Err = ParseHeadersError;
+    type Err = ParseHttpError;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         match StandardHeaders::from_str(s) {
-            std::result::Result::Ok(standard_header) => std::result::Result::Ok(HeaderKey::StandardHeader(standard_header)),
-            std::result::Result::Err(_) => std::result::Result::Ok(HeaderKey::CustomHeader(CustomHeader { 0: s.to_string() })),
+            std::result::Result::Ok(standard_header) => {
+                std::result::Result::Ok(HeaderKey::StandardHeader(standard_header))
+            }
+            std::result::Result::Err(_) => {
+                std::result::Result::Ok(HeaderKey::CustomHeader(CustomHeader { 0: s.to_string() }))
+            }
         }
     }
 }
-
 impl Display for HeaderKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -118,7 +135,6 @@ impl Display for HeaderKey {
         }
     }
 }
-
 impl Into<Vec<u8>> for HeaderKey {
     fn into(self) -> Vec<u8> {
         match self {
@@ -129,33 +145,179 @@ impl Into<Vec<u8>> for HeaderKey {
         }
     }
 }
-
-impl Into<Result<HeaderKey, ParseHeadersError>> for Vec<u8> {
-    fn into(self) -> Result<HeaderKey, ParseHeadersError> {
-        // let s = String::from_utf8_lossy(self.as_slice());
+impl Into<Result<HeaderKey, ParseHttpError>> for Vec<u8> {
+    fn into(self) -> Result<HeaderKey, ParseHttpError> {
         let s = match str::from_utf8(self.as_slice()) {
             std::result::Result::Ok(s) => s,
-            std::result::Result::Err(e) => return Err(ParseHeadersError("Invalid utf-8".to_string())),
+            std::result::Result::Err(_) => {
+                return Err(ParseHttpError::ParseHeaderError(
+                    "Invalid utf-8".to_string(),
+                ))
+            }
         };
         let result = match HeaderKey::from_str(s) {
             std::result::Result::Ok(result) => result,
-            std::result::Result::Err(e) => unreachable!(),
+            std::result::Result::Err(_) => unreachable!(),
+        };
+        Ok(result)
+    }
+}
+impl Into<Result<HeaderKey, ParseHttpError>> for String {
+    fn into(self) -> Result<HeaderKey, ParseHttpError> {
+        let result = match HeaderKey::from_str(self.as_str()) {
+            std::result::Result::Ok(result) => result,
+            std::result::Result::Err(_) => unreachable!(),
         };
         Ok(result)
     }
 }
 
-pub type HeaderValue = String;
+pub struct HeaderValue(String);
+impl Into<Vec<u8>> for HeaderValue {
+    fn into(self) -> Vec<u8> {
+        self.0.as_bytes().to_vec()
+    }
+}
+impl Into<Result<HeaderValue, ParseHttpError>> for String {
+    fn into(self) -> Result<HeaderValue, ParseHttpError> {
+        Ok(HeaderValue { 0: self })
+    }
+}
+impl Into<Result<HeaderValue, ParseHttpError>> for Vec<u8> {
+    fn into(self) -> Result<HeaderValue, ParseHttpError> {
+        match String::from_utf8(self) {
+            std::result::Result::Ok(value) => Ok(HeaderValue { 0: value }),
+            std::result::Result::Err(e) => Err(ParseHttpError::ParseHeaderError("Invalid utf-8 Value".to_string())),
+        }
+    }
+}
 
 pub struct Header {
     pub key: HeaderKey,
     pub value: HeaderValue,
 }
+impl Into<Vec<u8>> for Header {
+    fn into(self) -> Vec<u8> {
+        let mut result = Vec::new();
+        result.extend_from_slice(self.key.to_string().as_bytes());
+        result.extend_from_slice(": ".as_bytes());
+        result.extend_from_slice(self.value.0.as_bytes());
+        result
+    }
+}
+impl Into<Result<Header, ParseHttpError>> for Vec<u8> {
+    fn into(self) -> Result<Header, ParseHttpError> {
+        let data_str = match String::from_utf8(self) {
+            std::result::Result::Ok(result) => result,
+            std::result::Result::Err(_) => {
+                return Err(ParseHttpError::ParseHeaderError(
+                    "Invalid utf-8".to_string(),
+                ));
+            }
+        };
 
-struct FormData {
+        let separate_index = match data_str.find(':') {
+            Some(index) => index,
+            None => {
+                return Err(ParseHttpError::ParseHeaderError(format!(
+                    "{}: {}",
+                    "Invalid Header", data_str
+                )));
+            }
+        };
+
+        let key = match HeaderKey::from_str(&data_str[0..separate_index]) {
+            std::result::Result::Ok(key) => key,
+            std::result::Result::Err(e) => return Err(e),
+        };
+
+        let value: Result<HeaderValue, ParseHttpError> = data_str[separate_index + 1..].to_string().into()?;
+
+        Ok(Header { key, value })
+    }
+}
+
+pub struct FormData {
     pub boundary: String,
+    pub sections: Vec<FormDataSection>,
+}
+
+pub struct FormDataSection {
     pub headers: HashMap<HeaderKey, HeaderValue>,
     pub data: Vec<u8>,
+}
+
+impl FormData {
+    pub fn parse(boundary: String, raw_data: Vec<u8>) -> Result<FormData, ParseHttpError> {
+        let boundary_marker = format!("--{}", boundary);
+        let boundary_marker_bytes = boundary_marker.as_bytes();
+
+        let mut parts = Vec::new();
+        let mut start = 0;
+        while let Some(pos) = raw_data[start..]
+            .windows(boundary_marker.len())
+            .position(|chunk| chunk == boundary_marker_bytes)
+        {
+            parts.push(&raw_data[start..start + pos]);
+            start += pos + boundary_marker.len();
+        }
+        parts.push(&raw_data[start..]); // Add the remaining part
+
+        let mut data = Vec::new();
+        let formdata_sections: Vec<FormDataSection> = Vec::new();
+
+        for part in parts {
+            if part.is_empty() || part == b"--" {
+                continue; // Skip empty or terminating boundary
+            }
+            let body_separator = "\r\n\r\n".as_bytes();
+            let mut sections = part
+                .windows(body_separator.len())
+                .position(|chunk| chunk == body_separator)
+                .unwrap_or(part.len());
+            // let header_section = match sections.next(){
+            //     Some(header_section) => header_section,
+            //     None => "Missing header".as_bytes(),
+            // };
+            // let body_section = match sections.next(){
+            //     Some(body_section) => body_section,
+            //     None => "Missing body".as_bytes(),
+            // };
+
+            let header_section = &part[0..sections];
+            let body_section = &part[sections..part.len()];
+
+            let headers: HashMap<HeaderKey, HeaderValue> = HashMap::new();
+            for line in header_section.split(|&b| b == b'\n') {
+                if let (key, value) =
+                    line.split_at(line.iter().position(|&b| b == b':').unwrap_or(0))
+                {
+                    // let key = match str::from_utf8(key) {
+                    //     std::result::Result::Ok(key) => key,
+                    //     std::result::Result::Err(e) => {
+                    //         return Err(ParseHttpError::ParseHeaderError(
+                    //             "Invalid utf-8".to_string(),
+                    //         ))
+                    //     }
+                    // };
+                    // let value = match str::from_utf8(&value[1..]) {
+                    //     std::result::Result::Ok(value) => value,
+                    //     std::result::Result::Err(e) => {
+                    //         return Err(ParseHttpError::ParseHeaderError(
+                    //             "Invalid utf-8".to_string(),
+                    //         ))
+                    //     }
+                    // };
+                    headers.insert(key.into(), value);
+                }
+            }
+
+            // Add body data
+            data.push(body_section.to_vec());
+        }
+
+        Ok(())
+    }
 }
 
 pub struct Body {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-pub mod u24;
+#![feature(try_trait_v2_residual)]
+#![allow(try_trait_v2_residual)]pub mod u24;
 pub mod u31;
 
 pub mod http2;
+
+use std::ops;
+
 pub use http2::Http2Pri;
 
 pub mod http;
@@ -16,9 +20,28 @@ mod tests {
     fn it_works() {}
 }
 
-
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 pub enum Result<T, E> {
     Ok(T),
     Err(E),
+}
+impl<T, E> From<Result<T, E>> for std::result::Result<T, E> {
+    fn from(val: Result<T, E>) -> Self {
+        match val{
+            Result::Ok(value) => std::result::Result::Ok(value),
+            Result::Err(error) => std::result::Result::Err(error),
+        }
+    }
+}
+impl<T, E> From<std::result::Result<T, E>> for Result<T, E> {
+    fn from(val: std::result::Result<T, E>) -> Self {
+        match val{
+            std::result::Result::Ok(value) => Result::Ok(value),
+            std::result::Result::Err(error) => Result::Err(error),
+        }
+    }
+}
+#[unstable(feature = "try_trait_v2_residual", issue = "91285")]
+impl<T, E> ops::Residual<T> for Result<T, E> {
+    type TryType = Result<T, E>;
 }
